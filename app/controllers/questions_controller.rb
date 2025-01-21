50df522a059cafe337fa397275a9aa009794be44
Pavel <zelenkovpavlik@gmail.com>
2025-01-21 21:05:05 +0300

    Question controller with specs show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,18 @@
 class QuestionsController < ApplicationController
+
+  before_action :set_question, only: %i[show]
+
   def index
     @questions = Question.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
