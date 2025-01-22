ed66fd9b500bf34263c630d329d5e0e4e3f87690
Pavel <zelenkovpavlik@gmail.com>
2025-01-22 22:49:23 +0300

    Question controller with specs create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
 
-  before_action :set_question, only: %i[show edit]
+  before_action :load_question, only: %i[show edit]
 
   def index
     @questions = Question.all
@@ -16,9 +16,23 @@ class QuestionsController < ApplicationController
   def edit
   end
 
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   private
 
-  def set_question
+  def load_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
