dbda8f8f2f6d3e1cfd632bd14fd1b76b973d34b9
Pavel <zelenkovpavlik@gmail.com>
2025-08-13 18:56:25 +0300

    When editing a question/answer, the author can add new link

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -9,6 +9,7 @@ feature 'User can edit his answer', %q{
   given!(:user) { create_list(:user, 2) }
   given!(:question) { create(:question, author_id: user.first.id) }
   given!(:answer) { create(:answer, author_id: user.first.id, question_id: question.id) }
+  given!(:link) { create(:link, linkable: answer, name: "Link name", url: "https://gist.github.com") }
 
   scenario 'Unauthenticated can not edit answer' do
     visit question_path(question)
@@ -65,6 +66,39 @@ feature 'User can edit his answer', %q{
       end
       expect(page).to have_link 'rails_helper.rb'
       expect(page).to have_link 'spec_helper.rb'
+      # save_and_open_page
+    end
+
+    scenario 'edits his answer to add the link', js: true do
+      sign_in(user.first)
+      visit questions_path
+      click_on 'MyString'
+      click_on 'Edit'
+
+      within '.answers' do
+        click_on 'add link'
+        within all('.nested-fields').last do
+          fill_in 'Link name', with: 'My gist'
+          fill_in 'Url', with: 'https://gist.github.com/PavelZelenkov/0f47e461a01fb077db0824e2f2429a97'
+        end
+        click_on 'Save'
+      end
+      expect(page).to have_link('My gist', href: 'https://gist.github.com/PavelZelenkov/0f47e461a01fb077db0824e2f2429a97')
+      # save_and_open_page
+    end
+
+    scenario 'edits his answer to edit the link', js: true do
+      sign_in(user.first)
+      visit questions_path
+      click_on 'MyString'
+      click_on 'Edit'
+
+      within '.answers' do
+        fill_in 'Link name', with: 'My gist'
+        fill_in 'Url', with: 'https://gist.github.com/PavelZelenkov/0f47e461a01fb077db0824e2f2429a97'
+        click_on 'Save'
+      end
+      expect(page).to have_link('My gist', href: 'https://gist.github.com/PavelZelenkov/0f47e461a01fb077db0824e2f2429a97')
       # save_and_open_page
     end
   end
